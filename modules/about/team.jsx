3bf18cca935e74365619110639fefff9336df72b
Saqif Abrar <saqif.abrar@gmail.com>
2022-01-01 00:50:27 -0500

    updated pages

--- modules/about/team.jsx
+++ modules/about/team.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from "./team.module.scss";
 
 export default function team() {
-	return <div></div>;
+	return <div className={styles.teamContainer}></div>;
 }
